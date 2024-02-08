3192242a4cd4c08d3b98f74db2f9b821ddeda6cb
empiretylh <thuralinhtut3@gmail.com>
2024-02-08 16:19:52 +0630

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : '/thuralinhtut.github.io/',
+  base : '/empiretylh.github.io/',
   plugins: [react()],
   build:{
     outDir: 'build'
